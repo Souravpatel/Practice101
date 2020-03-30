cdbb9bc29703c91e1a65d9c0de421bc93ffd0c52
Souravpatel <sourav.patel997@gmail.com>
2020-03-30 11:13:34 +0530

    Initial version of demo

---
+++ Demo/src/demo1/Ex1.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		System.out.println("FOR DEMO PURPOSE");
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
