437d3ba14096732a8b73f87217dd6e4aa28049aa
Souravpatel <sourav.patel997@gmail.com>
2020-03-30 13:37:19 +0530

    123456 Commited message

---
+++ Demo/src/demo1/Ex2.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		System.out.println("FOR DEMO PURPOSE");
+		for(int i=10;i<=20;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
